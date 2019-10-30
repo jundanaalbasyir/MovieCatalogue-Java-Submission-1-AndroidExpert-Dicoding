e325914c49379296905a393b5ace8d0d2315ddf6
Jundana <jalbasyir@gmail.com>
2019-10-30 21:18:49 +0700

    Serializable to Parcelable

--- app/src/main/java/com/jundana/moviecatalogue/Model/Movie.java
+++ app/src/main/java/com/jundana/moviecatalogue/Model/Movie.java
@@ -1,8 +1,11 @@
 package com.jundana.moviecatalogue.Model;
 
-import java.io.Serializable;
+import android.annotation.SuppressLint;
+import android.os.Parcel;
+import android.os.Parcelable;
 
-public class Movie implements Serializable {
+@SuppressLint("ParcelCreator")
+public class Movie implements Parcelable {
     private int photo;
     private String movieName;
     private String movieDetail;
@@ -29,5 +32,17 @@ public class Movie implements Serializable {
 
     public void setMovieDetail(String movieDetail) {
         this.movieDetail = movieDetail;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(this.photo);
+        dest.writeString(this.movieName);
+        dest.writeString(this.movieDetail);
     }
 }
